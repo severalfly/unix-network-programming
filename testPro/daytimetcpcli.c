916a76468496124a6a1a2c2a183204b382404948
张云飞 <zhangsmile90@gmail.com>
2019-03-11 12:07:25 +0800

    修改编译输出

--- testPro/daytimetcpcli.c
+++ testPro/daytimetcpcli.c
@@ -34,7 +34,7 @@ int main(int argc, char const *argv[])
     {
         err_sys("connect error");
     }
-    fputs("aaab", stdout);
+    fputs("aaa", stdout);
 
     while( (n=read(sockfd, recvline, MAXLINE)) >0 )
     {
